ada3f4c0015bace3ba9f90b2c1e9bfe81f121da9
dw3624 <62541648+dw3624@users.noreply.github.com>
2021-03-18 16:44:43 +0900

    Add files via upload

--- 잡플래닛 크롤링.R
+++ 잡플래닛 크롤링.R
@@ -4,14 +4,15 @@
 library(httr) #요청
 library(rvest) #요청
 library(tidyverse) #정리
+library(stringr)
 
 # 로그인 화면의 URI를 복사하여 URI 객체에 지정합니다.
 URI <- 'https://www.jobplanet.co.kr/users/sign_in'
 
 # 로그인 정보를 이용하여 HTTP 요청을 합니다. 
 resp <- POST(url = URI,
-             body = list('user[email]' = '계정',
-                         'user[password]' = '비밀번호'))
+             body = list('user[email]' = 'berry1582@yonsei.ac.kr',
+                         'user[password]' = 'kang0905'))
 
 # 응답 상태코드를 확인합니다. 200이면 정상입니다.
 status_code(x = resp)
@@ -27,7 +28,7 @@ status_code(x=resp)
 #기업명
 resp %>% read_html() %>% html_node(css='body > div.body_wrap > div.cmp_hd > div.new_top_bnr > div > div.top_bnr_wrap > div > div > div.company_info_sec > div.company_info_box > div.company_name > h1 > a') %>% html_text()
 
-#날짜
+#작성날짜
 dates = resp %>% read_html() %>% html_nodes(css = 'div.content_top_ty2 > span.txt1 > span.txt2') %>% html_text()
 dates = gsub('[^0-9]','',dates)
 dates
@@ -56,21 +57,56 @@ myfunc = function(resp){
   
   #회사명
   company = resp %>% read_html() %>% html_node(css='body > div.body_wrap > div.cmp_hd > div.new_top_bnr > div > div.top_bnr_wrap > div > div > div.company_info_sec > div.company_info_box > div.company_name > h1 > a') %>% html_text()
+
+  #직종
   
+  #직급
+ # worker_data = resp %>% read_html() %>% html_nodes(css = 'div.content_top_ty2 > span.txt1') %>% html_text()
+ # worker_data = gsub(' {2,}','',class)
+ # worker_data = gsub('\n','',class)
+ # str_trim(worker_data)
+ # wdata_split = strsplit(class,split='\n')
+ # str_split(worker_data,split=' ')
+ # wdata_split
+ # class(wdata_split)
+ # dept=c(wdata_split[[]])
+ # rank=
+ # date=
+  
+
   #면접날짜
-  dates = resp %>% read_html() %>% html_nodes(css = 'div.content_top_ty2 > span.txt1 > span.txt2') %>% html_text()
-  dates = gsub('[^0-9]','',dates)
+  ##면접날짜가 없는 경우가 있어 큰 틀에서 숫자를 추출
+  int_date = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > dl') %>% html_text()
+  int_date = gsub('\n|[가-힣]','',int_date)
+  int_date = str_trim(int_date)
+    
+  #면접난이도
+  difficulty = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > dl > dd.blo_box2 > span') %>% html_text()
   
   #면접질문
   questions = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > dl > dd:nth-child(2) > span') %>% html_text()
-  questions = gsub(' {2,}','',questions)
-  questions = gsub('\n','',questions)
+  questions = str_trim(questions)
   
-  df_page=data.frame(company,dates,questions)
+  #면접답변 혹은 면접느낌
+  review = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > dl > dd:nth-child(4)') %>% html_text()
+  review = str_trim(review)
+
+  #면접결과
+  result = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > div.now_box > div > dl > dd:nth-child(2)') %>% html_text()
+  result = str_trim(result)
+
+  #면접경험
+  int_exp = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > div.now_box > div > dl > dd:nth-child(4)') %>% html_text()
+  
+  #도움이 돼요
+  helpful = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > div.btn_group > a.btn_help.helpful.btnval_on > span.notranslate') %>% html_text()
+  
+  df_page = data.frame(company, int_date, difficulty, questions, review, result, int_exp, helpful)
   
   return(df_page)
 }
-
+df_page=myfunc(resp)
+dim(df_page)
 
 # 반복문
 df_interview = data.frame()
@@ -87,9 +123,14 @@ for(page in 1:pages){
   
   resp = GET(url=url_page, config = list(cookies = myCookies))
   status_code(x=resp)
-  myfunc(resp)
+  df_page = myfunc(resp)
   df_interview = rbind(df_interview,df_page)
+  rm(resp,df_page)
 }
+
+#확인작업
+dim(df_interview) #총 리뷰수와 비교
+duplicated(x=df_interview$questions) %>% sum() #중복여부 확인
 
 #csv로 저장 및 불러오기, 확인
 write.csv(df_interview,'C:/Work_R/Others/interview.csv')
