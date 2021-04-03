e627b066a3d53bd6c170a62ffe68b80794b4a85b
dongwon_kang <62541648+dw3624@users.noreply.github.com>
2021-04-03 12:32:11 +0900

    Update 잡플래닛 크롤링.R
    코드 정리

--- 잡플래닛 크롤링.R
+++ 잡플래닛 크롤링.R
@@ -11,21 +11,22 @@ URI <- 'https://www.jobplanet.co.kr/users/sign_in'
 
 # 로그인 정보를 이용하여 HTTP 요청을 합니다. 
 resp <- POST(url = URI,
-             body = list('user[email]' = '계정',
-                         'user[password]' = '비밀번호'))
+             body = list('user[email]' = '본인의 계정을 입력',
+                         'user[password]' = '본인의 비밀번호를 '))
 
-# 응답 상태코드를 확인합니다. 200이면 정상입니다.
+# 응답 상태코드를 확인, 200이면 정상
 status_code(x = resp)
 
-# 쿠키만 수집하여 myCookies 객체에 할당합니다. 
-# 앞으로 HTTP 요청할 때 myCookies를 활용하면 로그인 상태로 HTML을 받을 수 있습니다. 
+# 쿠키만 수집해 myCookies 객체에 할당. 
+# 앞으로 HTTP 요청할 때 myCookies를 활용하면 로그인 상태로 HTML을 받을 수 있다. 
 myCookies <- set_cookies(.cookies = unlist(x = cookies(x = resp)))
 
-url_madup = '잡플라넷 기업 면접 페이지'
+url_company = '잡플라넷 기업 면접 페이지'
 # 예) https://www.jobplanet.co.kr/companies/000000/interviews
-resp = GET(url=url_madup, config = list(cookies = myCookies))
+resp = GET(url=url_company, config = list(cookies = myCookies))
 status_code(x=resp)
 
+##### ---- #####
 #기업명
 resp %>% read_html() %>% html_node(css='body > div.body_wrap > div.cmp_hd > div.new_top_bnr > div > div.top_bnr_wrap > div > div > div.company_info_sec > div.company_info_box > div.company_name > h1 > a') %>% html_text()
 
@@ -43,14 +44,15 @@ questions = resp %>% read_html() %>% html_nodes(css = 'div.ctbody_col2 > div > d
 questions = gsub(' {2,}','',questions)
 questions
 ## 원래경로: viewInterviewsList > div > div > section:nth-child(9) > div > div.ctbody_col2 > div > dl > dd:nth-child(2) > span
+##### ---- #####
+
 
 #마지막페이지
-##한페이지당 5건 밖에 보이지 않기 때문에 
+##한페이지당 5건 밖에 보이지 않기 때문
 interviewCnt = resp %>% read_html() %>% html_node(css='#viewCompaniesMenu > ul > li.viewInterviews > a > span') %>% html_text()
 interviewCnt = as.integer(interviewCnt)
 pages=ceiling(interviewCnt / 5)
 pages
-
 
 #함수
 ##개별요소들을 데이터프레임으로 변환
@@ -117,7 +119,7 @@ url_interview = 'https://www.jobplanet.co.kr/companies/000000/interviews'
 for(page in 1:pages){
   cat('[', page, '/', pages, '] 진행중... ',fill=TRUE)
   # 첫페이지 경우 url 그대로 반환
-  ## 첫페이지는 url뒤에 페이지 번호가 없다
+  ## 첫페이지는 url뒤에 페이지 번호가 없
   ifelse(page==1,
          assign('url_page',url_interview),
          assign('url_page',paste0(url_interview,'?page=',page))
@@ -135,6 +137,6 @@ dim(df_interview) #총 리뷰수와 비교
 duplicated(x=df_interview$questions) %>% sum() #중복여부 확인
 
 #csv로 저장 및 불러오기, 확인
-write.csv(df_interview,'C:/Work_R/Others/interview.csv')
-data=read.csv('C:/Work_R/Others/interview.csv')
+write.csv(df_interview,'경로 및 파일명')   # 예) 'C:/Work_R/Others/interview.csv'
+data = read.csv('경로 및 파일명')
 View(data)
